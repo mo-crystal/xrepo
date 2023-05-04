5941cc4e811e749c3ae988eb3ddaf8d042fd2904
Pokyux <shiftregister233@outlook.com>
2023-05-05 01:10:38 +0800

    Update xmake.lua

--- packages/m/mocutils/xmake.lua
+++ packages/m/mocutils/xmake.lua
@@ -1,5 +1,6 @@
 package("mocutils")
   set_urls("https://github.com/mo-crystal/mocutils.git")
   on_install("mingw", function (package)
-    os.vrun("xmake f -p mingw")
+    os.run("xmake f -p mingw")
+    os.run("xmake")
   end)
